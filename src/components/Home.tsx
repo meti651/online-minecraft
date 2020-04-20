3312503d8b36d15fdbc6f5209adde5be4ae3cdc0
Peti <meti651@gmail.com>
2020-04-20 17:14:38 +0200

    Home page base structure

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,23 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 interface Props {
 
 }
 
 const Home: React.FC<Props> = () => {
-    return <div>Home</div>;
+    return <div className="home-container">
+        <div className="home-title">
+            <h1>Online Minecraft</h1>
+        </div>
+        <div className="home-btns">
+            <Link to="/game/single" className="home-btn">Single Play</Link>
+            <Link to="/game/multi" className="home-btn">Multi Play</Link>
+            <Link to="/options" className="home-btn">Options</Link>
+            <Link to="/registration" className="home-btn">Sign Up</Link>
+            <Link to="/login" className="home-btn">Login</Link>
+        </div>
+    </div>;
 };
 
 export default Home;
