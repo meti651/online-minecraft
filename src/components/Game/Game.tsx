e75ffa990ca03b9c075275fbc922966f47290aa1
Peti <meti651@gmail.com>
2020-04-20 16:54:21 +0200

    base structure

---
+++ src/components/Game/Game.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface Props {
+
+}
+
+const Game: React.FC<Props> = () => {
+    return <div>Game</div>
+};
+
+export default Game;
---
+++ src/components/authentication/Registration.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface Props {
+
+}
+
+const Registration: React.FC<Props> = () => {
+    return <div>Registration</div>
+};
+
+export default Registration;
