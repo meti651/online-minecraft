d4d560e495da10c52fb13bf80f7091a7c2253912
Peti <meti651@gmail.com>
2020-04-22 11:43:55 +0200

    home page tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import App from './App';
+import {shallow, ShallowWrapper} from "enzyme";
+import Home from "./components/Home";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Router', () => {
+  test('renders App component', () => {
+    const wrapper = shallow(<App/>);
+    expect(wrapper.find('div').text()).toBe("<BrowserRouter />");
+  });
 });
+
+describe("Home Page", () => {
+  let wrapper:ShallowWrapper;
+  beforeEach(() => {wrapper = shallow(<Home/>)});
+
+  test("renders Title", () => {
+    expect(wrapper.find("h1").text()).toContain("Online Minecraft");
+  });
+
+  test("renders Links", () => {
+    expect(wrapper.find(".home-btns").children().length).toBe(5);
+  });
+});
+
