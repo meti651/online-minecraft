8f28c4ab964211db726e67313e8871f2bc0335aa
Peti <meti651@gmail.com>
2020-04-22 13:21:27 +0200

    designe part

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import {Link} from "react-router-dom";
 
+import "./Home.css";
+
 interface Props {
 
 }
@@ -8,14 +10,17 @@ interface Props {
 const Home: React.FC<Props> = () => {
     return <div className="home-container">
         <div className="home-title">
-            <h1>Online Minecraft</h1>
+            <div className="title">Online Minecraft</div>
         </div>
         <div className="home-btns">
             <Link to="/game/single" className="home-btn">Single Play</Link>
             <Link to="/game/multi" className="home-btn">Multi Play</Link>
             <Link to="/options" className="home-btn">Options</Link>
-            <Link to="/registration" className="home-btn">Sign Up</Link>
-            <Link to="/login" className="home-btn">Login</Link>
+            <div className="home-auth">
+                <Link to="/registration" className="home-btn">Sign Up</Link>
+                <div className="spacer"/>
+                <Link to="/login" className="home-btn">Login</Link>
+            </div>
         </div>
     </div>;
 };
