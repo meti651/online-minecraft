3214da5197d221cfbdd0b8a9198a7cbfafabde18
Peti <meti651@gmail.com>
2020-04-23 05:43:55 +0200

    test plane setup

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,17 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Home from "./components/Home";
 import Registration from "./components/authentication/Registration";
 import Login from "./components/authentication/Login";
-import Game from "./components/Game/Game";
+import Game from "./components/Game/Game.js";
+import BackGround from "./components/backGround/BackGround.js";
 
 function App() {
   return (
       <div className="main-container">
         <Router>
-          <Route exact path="/" component={Home} />
+          {/* HomePage*/}
+          {/*<Route exact path="/" component={Home} />*/}
+          {/* TestPage*/}
+          <Route exact path="/" component={BackGround} />
           <Route path="/registration" component={Registration} />
           <Route path="/login" component={Login} />
           <Route path="/game/{:type}" component={Game} />
