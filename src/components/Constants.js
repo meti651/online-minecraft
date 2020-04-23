0c1c3cca068f522b198ac6a0804a0a1c74d78468
Peti <meti651@gmail.com>
2020-04-23 05:50:48 +0200

    dunno

---
+++ src/components/Constants.js
@@ -0,0 +1,14 @@
+import BackArrow from "../public/images/back-arrow.png";
+import DirtBottom from "../public/images/Textures/dirt-bottom.jpg";
+import DirtTop from "../public/images/Textures/dirt-top.jpg";
+import DirtSide from "../public/images/Textures/dirt-side.jpg";
+
+export const images = {
+    backArrow: BackArrow
+};
+
+export const gameTextures = {
+    dirtBottom: DirtBottom,
+    dirtTop: DirtTop,
+    dirtSide: DirtSide,
+};
