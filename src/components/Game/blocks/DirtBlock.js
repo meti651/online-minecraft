9cf1450f6cbc618e9787fe3f9457d0b0cfac4023
Peti <meti651@gmail.com>
2020-04-23 05:42:33 +0200

    base block structure

--- src/components/Game/blocks/DirtBlock.js
+++ src/components/Game/blocks/DirtBlock.js
@@ -1,33 +1,21 @@
-import React, {useRef} from "react";
+import React, {useRef, useState} from "react";
+import {gameTextures} from "../../Constants";
 import {TextureLoader} from "three";
-import {textures} from "../../Constants";
 
 
-interface Props {
-    position: number[]
-}
+const DirtBlock = ({position}) => {
+    const [textures] = useState(() => [gameTextures.dirtSide, gameTextures.dirtSide, gameTextures.dirtTop, gameTextures.dirtBottom,  gameTextures.dirtSide, gameTextures.dirtSide].map(texture => {
+        return new TextureLoader().load(texture);
+    }));
 
-const DirtBlock:React.FC<Props> = ({position}) => {
     const mesh = useRef();
 
-    const loader = new TextureLoader();
-    const top = loader.load(textures.dirtTop);
-    const bottom = loader.load(textures.dirtBottom);
-    const side = loader.load(textures.dirtSide);
-
-    const materials = [
-        <meshPhysicalMaterial key={"side1"} attach="material" map={side}/>,
-        <meshPhysicalMaterial key={"bottom"} attach="material" map={bottom}/>,
-        <meshPhysicalMaterial key={"side2"} attach="material" map={side}/>,
-        <meshPhysicalMaterial key={"side3"} attach="material" map={side}/>,
-        <meshPhysicalMaterial key={"side4"} attach="material" map={side}/>,
-        <meshPhysicalMaterial key={"top"} attach="material" map={top}/>
-    ];
-    
-    return <mesh {...position} ref={mesh} castShadow>
-        {materials.map(material => material)}
-        <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-    </mesh>
+    return (
+            <mesh {...position} ref={mesh} castShadow >
+                <boxBufferGeometry attach="geometry" args={[1, 1, 1]}/>
+                {textures.map(texture => <meshLambertMaterial key={texture.uuid} attachArray="material" color='white' map={texture}/>)}
+            </mesh>
+    )
 };
 
 export default DirtBlock;
