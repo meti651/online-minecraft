350985ca6556b95d0dabf9845a9a6dcc3b416131
Peti <meti651@gmail.com>
2020-04-23 05:45:31 +0200

    first menu background structure

--- src/components/backGround/BackGround.js
+++ src/components/backGround/BackGround.js
@@ -1,20 +1,21 @@
-import React from "react";
+import React, {Suspense} from "react";
 import {Canvas} from "react-three-fiber";
 import DirtBlock from "../Game/blocks/DirtBlock";
 import Ground from "./Ground";
 import Controls from "./Controls";
 
-interface Props {
 
-}
 
-const BackGround: React.FC<Props> = () => {
+const BackGround = () => {
     return <Canvas camera={{position: [5, 5, 5], rotation: [0, 0, 90]}} shadowMap>
-        <Controls autoRotate enablePan={true} enableZoom={true} enableDamping dampingFactor={0.5} rotateSpeed={1}/>
+        {/*<fog attach="fog" args={["white", 10, 30]}/>*/}
+        <Controls autoRotate />
         <ambientLight intensity={0.75}/>
         <pointLight intensity={0.25} position={[5, 0, 5]}/>
         <spotLight position={[-5, 2.5, 5]} intensity={0.25} penumbra={1} castShadow/>
-        <DirtBlock position={[0, -0.5, 0]}/>
+        <Suspense fallback={null}>
+            <DirtBlock position={[0, -0.5, 0]}/>
+        </Suspense>
         <Ground/>
     </Canvas>
 };
--- src/components/backGround/Ground.js
+++ src/components/backGround/Ground.js
@@ -1,12 +1,9 @@
 import React from "react";
 
-interface Props {
 
-}
-
-const Ground:React.FC<Props> = () => {
-    return <mesh position={[0, -1, 0]} rotation={[-Math.PI/2, 0, 0]} receiveShadow>
-        <planeBufferGeometry attach="geometry" args={[100, 100, 100]} />
+const Ground = () => {
+    return <mesh position={[0, -0.5, 0]} rotation={[-Math.PI/2, 0, 0]} receiveShadow>
+        <planeBufferGeometry attach="geometry" args={[1000, 1000]} />
         <meshPhysicalMaterial attach="material" color={'brown'} />
     </mesh>
 };
