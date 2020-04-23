a8f9f6c30f3cc817359bf1c7ce8b55885f10e57e
Peti <meti651@gmail.com>
2020-04-23 14:44:13 +0200

    plane place in the canvas

--- src/components/backGround/BackGround.js
+++ src/components/backGround/BackGround.js
@@ -1,20 +1,21 @@
 import React, {Suspense} from "react";
-import {Canvas} from "react-three-fiber";
+import {Canvas, useFrame} from "react-three-fiber";
 import DirtBlock from "../Game/blocks/DirtBlock";
 import Ground from "./Ground";
 import Controls from "./Controls";
+import Plane from "./Plane";
 
 
 
 const BackGround = () => {
-    return <Canvas camera={{position: [5, 5, 5], rotation: [0, 0, 90]}} shadowMap>
+
+    return <Canvas camera={{position: [5, 5, 5], rotation: [0, 0, 90]}} invalidateFrameloop shadowMap>
         {/*<fog attach="fog" args={["white", 10, 30]}/>*/}
         <Controls autoRotate />
         <ambientLight intensity={0.75}/>
-        <pointLight intensity={0.25} position={[5, 0, 5]}/>
-        <spotLight position={[-5, 2.5, 5]} intensity={0.25} penumbra={1} castShadow/>
+        <pointLight castShadow position={[0, 10, 0]} intensity={0.5}/>
         <Suspense fallback={null}>
-            <DirtBlock position={[0, -0.5, 0]}/>
+            <Plane/>
         </Suspense>
         <Ground/>
     </Canvas>
