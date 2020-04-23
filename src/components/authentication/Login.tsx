e14382e40333544fddf16f05a0c4d35b07d9b764
Peti <meti651@gmail.com>
2020-04-23 14:46:45 +0200

    endpoints source changed

--- src/components/authentication/Login.tsx
+++ src/components/authentication/Login.tsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from "react";
 import {useHistory} from "react-router-dom";
 import axios from "axios";
-import {endpoints} from "./AuthenticationPaths";
+import {endpoints} from "../ApiEndpoints";
 
 import "./Authentication.css";
 import BackStepper from "../BackStepper";
