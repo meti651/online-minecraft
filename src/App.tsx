97ccd3c65a87552230653fff124dfd202a7501de
Peti <meti651@gmail.com>
2020-05-05 10:00:07 +0200

    setup the player scene canvas - canvas conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Home from "./components/Home";
@@ -8,17 +8,18 @@ import Game from "./components/Game/Game.js";
 import BackGround from "./components/backGround/BackGround.js";
 
 function App() {
+    const [isPlaying, setIsPlaying] = useState(false);
   return (
       <React.Fragment>
-      <BackGround/>
-      <div className="main-container">
-          <Router>
-            <Route exact path="/" component={Home} />
-            <Route path="/registration" component={Registration} />
-            <Route path="/login" component={Login} />
-            <Route path="/game/{:type}" component={Game} />
-        </Router>
-      </div>
+          {/*<BackGround/>*/}
+          {/*<div className="main-container">*/}
+              <Router>
+                  <Route exact path="/" component={() => <Home setPlaying={setIsPlaying}/>} />
+                  <Route path="/registration" component={Registration} />
+                  <Route path="/login" component={Login} />
+                  <Route path="/game/:type" component={Game}/>
+              </Router>
+          {/*</div>*/}
       </React.Fragment>
   );
 }
