c589578ef4f3bc0593c55bdf81e4e2ffb6bab02f
Peti <meti651@gmail.com>
2020-05-07 12:07:57 +0200

    different scene-s

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, useState} from "react";
+import React, {useEffect, useState} from "react";
 import SingleScene from "./Scenes/SingleScene";
 import MultiScene from "./Scenes/MultiScene";
 
